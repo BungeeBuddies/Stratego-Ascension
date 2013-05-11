9b51e0e03507c84ce70f9a84bc7019ec8a40df69
Jacko Dirks <jacko.dirks@gmail.com>
2013-05-11 23:47:15 +0200

    Wrote some code, doesnt work

---
+++ main.py
@@ -0,0 +1,5 @@
+import stuk
+
+print "Hello, World!"
+stuk = Stuk(1)
+stuk.printType
---
+++ stuk.py
@@ -0,0 +1,7 @@
+class Stuk:
+	type = -1
+	def __init__(self,type):
+		self.type = type
+
+	def printType(self):
+		print self.type
