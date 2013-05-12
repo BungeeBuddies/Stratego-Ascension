b5476b94ea321406cd52e1723fc93a1563899f5a
Marcelo Dias Avelino <valorcurse@valorcurse-Laptop.(none)>
2013-05-12 16:11:48 +0200

    Added basic functionality.

--- main.py
+++ main.py
@@ -1,5 +1,53 @@
-import stuk
+import pyglet
+from pyglet.gl import *
+from math import pi, sin, cos
+from time import time
+from copy import deepcopy
 
-print "Hello, World!"
-stuk = Stuk(1)
-stuk.printType
+class Window(pyglet.window.Window):
+
+	def __init__(self):
+		super(Window, self).__init__()
+		self.set_size(800, 600)
+
+	def on_key_press(self, symbol, modifiers):
+		pass
+
+	def on_mouse_release(self, x, y, button, modifiers):
+		pass
+
+	def on_mouse_press(self, x, y, button, modifiers):
+		pass
+					
+	def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+		pass
+
+
+	def on_mouse_motion(self, x, y, dx, dy):
+		pass
+
+
+	def drawPoint(self, x, y, color):
+		pyglet.graphics.draw(1, GL_POINTS,
+			('v2i', (x, y)),
+			('c3B', (color[0], color[1], color[2])))
+
+	def circle(self, x, y, radius, color):
+		smoothness = int(2*radius*pi)
+		glBegin(GL_TRIANGLE_FAN)
+		glColor3f(color[0], color[1], color[2])
+		for i in range(0, smoothness):
+			angle = i * pi * 2.0 / smoothness
+			glVertex2f(x + radius * cos(angle), y + radius * sin(angle))
+			glEnd()
+
+	def on_draw(self):
+		pass
+
+	def update(self, dt):
+		pass
+
+if __name__ == '__main__':
+	window = Window()
+	pyglet.clock.schedule_interval(window.update, 1.0/60.0)
+	pyglet.app.run()
--- stuk.py
+++ stuk.py
@@ -1,5 +1,6 @@
 class Stuk:
 	type = -1
+
 	def __init__(self,type):
 		self.type = type
 
