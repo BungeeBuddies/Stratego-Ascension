aae32b927c1cae910ebb5d0dff78f378ea987450
Marcelo Dias Avelino <valorcurse@marcelo-Mint.(none)>
2013-05-13 00:14:27 +0200

    Creating and drawing the playfield now possible.

---
+++ field.py
@@ -0,0 +1,6 @@
+class Field:
+
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.size = 45
--- main.py
+++ main.py
@@ -5,12 +5,18 @@ from pyglet.gl import *
 from math import pi, sin, cos
 from time import time
 from copy import deepcopy
+from field import Field
 
 class Window(pyglet.window.Window):
 
 	def __init__(self):
 		super(Window, self).__init__()
 		self.set_size(800, 600)
+		self.sizeOfField = 10
+		self.xOffset = 175
+		self.yOffset = 100
+
+		self.fields = self.createPlayField()
 
 	def on_key_press(self, symbol, modifiers):
 		pass
@@ -34,7 +40,7 @@ class Window(pyglet.window.Window):
 			('v2i', (x, y)),
 			('c3B', (color[0], color[1], color[2])))
 
-	def circle(self, x, y, radius, color):
+	def drawcircle(self, x, y, radius, color):
 		smoothness = int(2*radius*pi)
 		glBegin(GL_TRIANGLE_FAN)
 		glColor3f(color[0], color[1], color[2])
@@ -43,12 +49,53 @@ class Window(pyglet.window.Window):
 			glVertex2f(x + radius * cos(angle), y + radius * sin(angle))
 			glEnd()
 
+	def createPlayField(self):
+		fields = [[Field(0, 0) for x in xrange(self.sizeOfField)] for y in xrange(self.sizeOfField)]
+
+		for y in range(0, len(fields)):
+			for x in range(0, len(fields[0])):
+				fields[y][x].x = x * fields[y][x].size + self.xOffset
+				fields[y][x].y = y * fields[y][x].size + self.yOffset
+
+		return fields
+
+	def drawPlayField(self):
+		print "drawing"
+		
+
+	def drawPlayField(self):
+		pass
+
 	def on_draw(self):
 		pass
 
 	def update(self, dt):
-		pass
+		for y in range(0, len(self.fields)):
+			for field in self.fields[y]:
+				# Draw top side
+				glColor3f(0, 1, 0)
+				pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2i', 
+    				(field.x + field.size, field.y + field.size, 
+    				field.x - field.size, field.y + field.size)))
 
+				# Draw down side
+				glColor3f(0, 0, 1)
+				pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2i', 
+    				(field.x + field.size, field.y - field.size, 
+    				field.x - field.size, field.y - field.size)))
+				
+				# Draw left side
+				glColor3f(1, 1, 0)
+				pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2i', 
+    				(field.x - field.size, field.y - field.size,
+    				field.x - field.size, field.y + field.size)))
+
+				# Draw right side
+				glColor3f(1, 1, 1)
+				pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2i', 
+    				(field.x + field.size, field.y - field.size,
+    				field.x + field.size, field.y + field.size)))
+				
 if __name__ == '__main__':
 	window = Window()
 	pyglet.clock.schedule_interval(window.update, 1.0/60.0)
